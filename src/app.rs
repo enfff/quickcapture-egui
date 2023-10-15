222562e1ab24c04eaf901fdc37a001d8337345f5
enfff <enforcetitan@gmail.com>
2023-10-15 17:02:24 +0200

    first commit :)

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,27 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+
+pub struct QuickCaptureApp {
     // Example stuff:
     label: String,
-
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+    button_spacing: f32
 }
 
-impl Default for TemplateApp {
+impl Default for QuickCaptureApp {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            button_spacing: 4.0,
         }
     }
 }
 
-impl TemplateApp {
+impl QuickCaptureApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -35,7 +37,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for QuickCaptureApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -66,19 +68,38 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            // ui.heading("eframe template");
 
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
                 ui.text_edit_singleline(&mut self.label);
             });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            // ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            // if ui.button("Increment").clicked() {
+            //     self.value += 1.0;
+            // }
 
             ui.separator();
+
+            ui.horizontal(|ui| {
+                if ui.small_button("💾 Save").clicked() {
+                    println!("Save button pressed")
+                }
+
+                ui.add_space(self.button_spacing);
+
+                if ui.small_button("↖ Arrow").hovered() {
+                    println!("Hovering on arrow button")
+                }
+
+                ui.add_space(self.button_spacing);
+
+                if ui.small_button("| Line").clicked() {
+                    println!("Pressed line button")
+                }
+
+            });
 
             ui.add(egui::github_link_file!(
                 "https://github.com/emilk/eframe_template/blob/master/",
--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,13 @@ fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
-        initial_window_size: Some([400.0, 300.0].into()),
-        min_window_size: Some([300.0, 220.0].into()),
+        initial_window_size: Some([600.0, 400.0].into()),
+        min_window_size: Some([300.0, 200.0].into()),
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Quickcapture",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(quickcapture::QuickCaptureApp::new(cc))),
     )
-}
-
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
-            .start(
-                "the_canvas_id", // hardcode it
-                web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
 }
