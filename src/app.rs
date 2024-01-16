45975e60461a02f611d513dcfc463024044436bb
Daniele De Rossi <daniele.dr00@gmail.com>
2024-01-16 15:25:30 +0100

    screenshot parziale funzionante

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
-use egui::Button;
-use image::{ImageBuffer, RgbaImage};
+use egui::*;
+use image::RgbaImage;
 use std::sync::mpsc;
 use std::{thread, time};
 
@@ -8,11 +8,11 @@ mod painting_utils;
 mod pathlib;
 mod save_utils;
 mod screenshot_utils;
+mod screenshot_view;
 
 use crate::app::save_utils::SavePath;
 
 use self::save_utils::check_filename;
-
 
 pub enum Views {
     Home,
@@ -21,7 +21,7 @@ pub enum Views {
     Save,
 }
 
-#[derive(Clone, Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq, Debug)]
 pub enum ScreenshotType {
     FullScreen,
     PartialScreen,
@@ -42,6 +42,7 @@ pub struct QuickCaptureApp {
     painted_screenshot: Option<egui::TextureHandle>, // egui wants TextureHandles for painting on things. However, this cannot be used to save the image.
     pub took_new_screenshot: bool,
     pub save_path: SavePath,
+    screenshot_view: screenshot_view::ScreenshotView,
 }
 
 impl Default for QuickCaptureApp {
@@ -57,6 +58,7 @@ impl Default for QuickCaptureApp {
                 std::env::current_dir().unwrap().join("target"),
                 ImgFormats::PNG,
             ), // Salva in <app_directory>/target/
+            screenshot_view: screenshot_view::ScreenshotView::new(),
         }
     }
 }
@@ -142,7 +144,6 @@ impl QuickCaptureApp {
     pub fn settings_view(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         // Will contain the shortcuts
         egui::CentralPanel::default().show(ctx, |ui| {
-            println!("settings_view");
             ui.label("Settings view");
             if ui.button("go back").clicked() {
                 self.view = Views::Home;
@@ -164,22 +165,34 @@ impl QuickCaptureApp {
 
         // Prima hai scelto che screenshot fare, adesso fai lo screenshot
         // Questa parte è stata anticipata altrimenti si vedrebbe la maschera disegnata nelle righe successive
+        self.screenshot_view.ui(ctx, _frame, &mut self.view, &mut self.screenshot_type);
         if self.screenshot_type.is_some() {
             // It's not the screenshot, but the data describing it. It needs to be converted to an image.
-
+            _frame.set_window_size(vec2(640.0, 400.0));
+            _frame.set_centered();
+            _frame.set_visible(false);
+            ctx.request_repaint();
             // quick and dirty solution, not too proud but i couldn't find any  other way around it...
             thread::sleep(time::Duration::from_millis(150));
-
-            let (tx_screenshot_buffer, rx_screenshot_buffer) =
-                mpsc::channel::<Option<ImageBuffer<image::Rgba<u8>, Vec<u8>>>>();
+            let (tx_screenshot_buffer, rx_screenshot_buffer) = mpsc::channel();
             let tmp_screenshot_type = self.screenshot_type.clone();
-
-            // Take the screenshot and wait until it's done
-            thread::spawn(move || {
-                let screenshot_image_buffer =
-                    screenshot_utils::take_screenshot(tmp_screenshot_type);
-                tx_screenshot_buffer.send(screenshot_image_buffer).unwrap();
-            });
+            let ctx1 = ctx.clone();
+            if self.screenshot_type.clone().unwrap() == ScreenshotType::FullScreen {
+                // Take the screenshot and wait until it's done
+                thread::spawn(move || {
+                    let screenshot_image_buffer =
+                        screenshot_utils::take_screenshot(tmp_screenshot_type, None, &ctx1);
+                    tx_screenshot_buffer.send(screenshot_image_buffer).unwrap();
+                });
+            } else if self.screenshot_type.clone().unwrap() == ScreenshotType::PartialScreen {
+                //TODO
+                let grab = self.screenshot_view.clone();
+                thread::spawn(move || {
+                    let screenshot_image_buffer =
+                        screenshot_utils::take_screenshot(tmp_screenshot_type, Some(grab), &ctx1);
+                    tx_screenshot_buffer.send(screenshot_image_buffer).unwrap();
+                });
+            }
 
             self.screenshot_image_buffer = rx_screenshot_buffer.recv().unwrap();
 
@@ -204,51 +217,18 @@ impl QuickCaptureApp {
             _frame.set_visible(true);
 
             // Maschera sopra lo schermo per scegliere il tipo di screenshot
-            egui::Window::new("screenshot_view")
-                .title_bar(false)
-                .fixed_pos(egui::pos2(0.0, 0.0))
-                .show(ctx, |ui| {
-                    // self.id = Some(ui.layer_id());
-                    ui.horizontal(|ui| {
-                        ui.horizontal(|ui| {
-                            if ui.button("⛶").clicked() {
-                                self.screenshot_type = Some(ScreenshotType::PartialScreen);
-                                println!("PartialScreen button pressed");
-                            }
-                            ui.separator();
-
-                            if ui.button("🖵").clicked() {
-                                self.screenshot_type = Some(ScreenshotType::FullScreen);
-                                println!("FullScreen button pressed");
-                            }
-                            ui.separator();
-
-                            if ui.button("◀").clicked() {
-                                // restore_dim(&None, _frame, Some(Views::Home));
-                                self.view = Views::Home;
-                            }
-
-                            if self.screenshot_type.is_some() {
-                                // L'utente ha scelto che screenshot da fare
-                                println!("screenshot_type is some");
-
-                                // Hides the screen
-                                _frame.set_visible(false);
-                                ui.set_visible(false);
-                                ctx.request_repaint();
-                            }
-                        });
-                    });
-                });
-        }
+            
     }
+}
+
 
     pub fn save_view(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             // println!("settings_view");
             ui.label("Save view");
             pathlib::ui(ui, &mut self.save_path);
-            let save_button = ui.add_enabled(check_filename(&self.save_path.name), Button::new("Save"));
+            let save_button =
+                ui.add_enabled(check_filename(&self.save_path.name), Button::new("Save"));
             if save_button.clicked() {
                 println!("Save button pressed");
                 save_utils::save_image(
@@ -257,10 +237,12 @@ impl QuickCaptureApp {
                 );
                 self.view = Views::Home;
             };
-            
+
             if ui.button("Go back").clicked() {
                 self.view = Views::Home;
             };
         });
     }
+
 }
+
--- src/app/screenshot_utils.rs
+++ src/app/screenshot_utils.rs
@@ -1,5 +1,6 @@
+use super::screenshot_view::ScreenshotView;
 use crate::app::ScreenshotType;
-use image::{GenericImage, RgbaImage};
+use image::{GenericImage, RgbaImage, GenericImageView};
 use screenshots::Screen;
 use std::io::Cursor;
 
@@ -9,61 +10,60 @@ struct ScreenImage {
 }
 
 // Fa lo screenshot di ogni schermo e ritorna un buffer che descrive l'immagine. Ma non è l'immagine stessa.
-pub fn take_screenshot(_screenshot_type: Option<ScreenshotType>) -> Option<image::RgbaImage> {
-
+pub fn take_screenshot(
+    _screenshot_type: Option<ScreenshotType>,
+    _grabbed_area: Option<ScreenshotView>,
+    _ctx: &egui::Context,
+) -> Option<image::RgbaImage> {
     // TODO
     // Virtual desktops don't count as screens
-    if _screenshot_type == Some(ScreenshotType::FullScreen) {
-        let mut img: RgbaImage;
+    let mut img: RgbaImage;
+    let screen_images = Screen::all()
+        .unwrap()
+        .into_iter()
+        .map(|screen| {
+            let image = screen.capture().unwrap();
+            ScreenImage { screen, image }
+        })
+        .collect::<Vec<ScreenImage>>();
+    let x_min = screen_images
+        .iter()
+        .map(|s| s.screen.display_info.x * s.screen.display_info.scale_factor as i32)
+        .min()
+        .unwrap();
+    let y_min = screen_images
+        .iter()
+        .map(|s| s.screen.display_info.y * s.screen.display_info.scale_factor as i32)
+        .min()
+        .unwrap();
+    let x_max = screen_images
+        .iter()
+        .map(|s| (s.screen.display_info.x + s.screen.display_info.width as i32) * s.screen.display_info.scale_factor as i32)
+        .max()
+        .unwrap();
+    let y_max = screen_images
+        .iter()
+        .map(|s| (s.screen.display_info.y + s.screen.display_info.height as i32) * s.screen.display_info.scale_factor as i32)
+        .max()
+        .unwrap();
 
-        let screen_images = Screen::all()
-            .unwrap()
-            .into_iter()
-            .map(|screen| {
-                let image = screen.capture().unwrap();
-                ScreenImage { screen, image }
-            })
-            .collect::<Vec<ScreenImage>>();
-        let x_min = screen_images
-            .iter()
-            .map(|s| s.screen.display_info.x)
-            .min()
-            .unwrap();
-        let y_min = screen_images
-            .iter()
-            .map(|s| s.screen.display_info.y)
-            .min()
-            .unwrap();
-        let x_max = screen_images
-            .iter()
-            .map(|s| s.screen.display_info.x + s.screen.display_info.width as i32)
-            .max()
-            .unwrap();
-        let y_max = screen_images
-            .iter()
-            .map(|s| s.screen.display_info.y + s.screen.display_info.height as i32)
-            .max()
-            .unwrap();
-
-        let offset = (x_min, y_min);
-        let size = ((x_max - x_min) as u32, (y_max - y_min) as u32);
-        println!("Total screenshot size: {:?}", size);
-        println!("Offset: {:?}", offset);
-
-        img = RgbaImage::new(size.0, size.1);
-        for pixels in img.enumerate_pixels_mut() {
-            *pixels.2 = image::Rgba([0, 0, 0, 255]);
-        }
-        for screen_image in screen_images {
-            println!("Screen: {:?}", screen_image.screen.display_info.id);
-
-            let mut screenshot =
-                image::io::Reader::new(Cursor::new(screen_image.image.to_png().unwrap()))
-                    .with_guessed_format()
-                    .unwrap()
-                    .decode()
-                    .unwrap();
-            if screen_image.screen.display_info.scale_factor != 1.0 {
+    let offset = (x_min, y_min);
+    println!("Offset: {:?}", offset);
+    let size: (u32, u32);
+    size = ((x_max - x_min) as u32, (y_max - y_min) as u32);
+    println!("Size: {:?}", size);
+    img = RgbaImage::new(size.0, size.1);
+    for pixels in img.enumerate_pixels_mut() {
+        *pixels.2 = image::Rgba([0, 0, 0, 255]);
+    }
+    for screen_image in screen_images {
+        println!("Screen: {:?}", screen_image.screen.display_info);
+            let screenshot = image::io::Reader::new(Cursor::new(screen_image.image.to_png().unwrap()))
+                .with_guessed_format()
+                .unwrap()
+                .decode()
+                .unwrap();
+            /*if screen_image.screen.display_info.scale_factor != 1.0 {
                 println!(
                     "Scale factor: {}",
                     screen_image.screen.display_info.scale_factor
@@ -76,35 +76,50 @@ pub fn take_screenshot(_screenshot_type: Option<ScreenshotType>) -> Option<image
                     image::imageops::FilterType::Nearest,
                 );
                 screenshot = scaled_screenshot;
-            }
+            }*/
 
-            let x = (screen_image.screen.display_info.x - offset.0) as u32;
-            let y = (screen_image.screen.display_info.y - offset.1) as u32;
-
+            let x = (screen_image.screen.display_info.x * screen_image.screen.display_info.scale_factor as i32 - offset.0) as u32;
+            let y = (screen_image.screen.display_info.y * screen_image.screen.display_info.scale_factor as i32 - offset.1) as u32;
             if x + screenshot.width() <= img.width() && y + screenshot.height() <= img.height() {
-                match img.copy_from(&screenshot, x, y) {
-                    Ok(_) => (),
-                    Err(e) => println!("Failed to copy screen image: {}", e),
-                }
-            } else {
-                println!("Screen image is out of bounds: {:?}", screen_image.screen);
-            }
-        }
-
-        // TODO gestisci meglio questa situazione: fai che sia una Option<,> prima, così vedi se è andato a buon fine ritorna solo con esito positivo
-        let return_img: Option<RgbaImage> = Some(img);
-        if return_img.is_some() {
-            println!("Screenshot taken");
-            return return_img;
-        } else {
-            println!("Screenshot failed");
-            return None;
-        }
-
+                    match img.copy_from(&screenshot, x, y) {
+                        Ok(_) => (),
+                        Err(e) => println!("Failed to copy screen image: {}", e),
+                    }
+            }   
     }
-    else if _screenshot_type == Some(ScreenshotType::PartialScreen){
-
+    if _screenshot_type.clone().unwrap() == ScreenshotType::PartialScreen{
+        let grab = _grabbed_area.clone().unwrap();
+        let x_start: i32;
+        let y_start: i32;
+        println!("Pixels per point: {}", _ctx.pixels_per_point());
+        if grab.starting_point.x > grab.ending_point.x {
+            x_start = (grab.ending_point.x * _ctx.pixels_per_point()) as i32 as i32 - offset.0 as i32;
+        } else {
+            x_start = (grab.starting_point.x * _ctx.pixels_per_point()) as i32 as i32 - offset.0 as i32;
+        }
+        if grab.starting_point.y < grab.ending_point.y {
+            y_start = (grab.starting_point.y * _ctx.pixels_per_point()) as i32 as i32 - offset.1 as i32;
+        } else {
+            y_start = (grab.ending_point.y * _ctx.pixels_per_point()) as i32 as i32 - offset.1 as i32;
+        }
+        let clone = img.clone();
+        println!("grab dimensions: {:?}", grab.dimension_selected);
+        let (real_x, real_y) = (grab.dimension_selected.x * _ctx.pixels_per_point(), grab.dimension_selected.y * _ctx.pixels_per_point());
+        let cropped = image::imageops::crop_imm(&clone, x_start as u32, y_start as u32, real_x as u32, real_y as u32);
+        println!("cropped dimensions: {:?}", cropped.dimensions());
+        let cropped_to_img = cropped.to_image();
+        img = RgbaImage::new(real_x as u32, real_y as u32);
+        match img.copy_from(&cropped_to_img, 0, 0) {
+            Ok(_) => (),
+            Err(e) => println!("Failed to copy screen image: {}", e),
+        }
+    }
+    let return_img: Option<RgbaImage> = Some(img);
+    if return_img.is_some() {
+        println!("Screenshot taken");
+        return return_img;
+    } else {
+        println!("Screenshot failed");
         return None;
     }
-    return None;
 }
